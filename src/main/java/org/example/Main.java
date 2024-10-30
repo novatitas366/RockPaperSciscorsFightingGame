ff7ea57573e28fd840a12225080d960b5f92457e
novatitas366 <novatitas366@gmail.com>
2024-10-30 14:03:49 +0200

    added an algorithm to parse json files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
+package org.example;
 
-package org.example; 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Menu.ShowMenu();
+        
+        Gson gson = new Gson();
+        LinkedList<Integer> list = new LinkedList<>();
+        list.
+        try (FileReader reader = new FileReader("src/main/java/org/example/data.json")) {
+            Person person = gson.fromJson(reader, Person.class);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+            System.out.println("Email: " + person.getEmail());
+        } catch (JsonSyntaxException | JsonIOException | IOException e) {
+            e.printStackTrace();
+        }
+
+        
     }
 }
