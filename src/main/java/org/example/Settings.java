7b087d7c2f6e37f25a34fdf3102da2a3cb18fbcc
novatitas366 <novatitas366@gmail.com>
2024-11-05 17:05:40 +0200

    added a constructor to the settings class

--- src/main/java/org/example/Settings.java
+++ src/main/java/org/example/Settings.java
@@ -1,10 +1,16 @@
 package org.example;
 
 public class Settings {
-    class settings{
+    class settings {
         private String aiDifficulty;
         private int menuSpeed;
-        
+        private int basicAttackDamage;
+
+        public settings(String aiDifficulty, int menuSpeed, int basicAttackDamage){
+            this.aiDifficulty = aiDifficulty;
+            this.menuSpeed = menuSpeed;
+            this.basicAttackDamage = basicAttackDamage;
+        }
     }
     
 }
